702e66c16e4848e0d238355d9060a2e958996052
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-11-22 13:15:57 -0600

    Through Chapter 13

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,13 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
 import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./KandyKorner.css";
 
-export const KandyKorner = () => {
-    return (
-        <>
-            <NavBar />
-            <h1>Kandy Korner's Tasty Treats</h1>
-            <ApplicationViews />
-        </>
-    )
-}
+
+export const KandyKorner = () => (
+    <>
+      <Route
+        render={() => {
+          if (localStorage.getItem("kandy_customer")) {
+            return (
+              <>
+                <NavBar />
+                <h1>Kandy Korner's Tasty Treats</h1>
+                <ApplicationViews />
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+  
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </>
+  );
